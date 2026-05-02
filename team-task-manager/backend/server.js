fe5056e8a21db35f6eeaa4ed66e3c626baa50bbf
gulshan <gk0507818@gmail.com>
2026-05-02 15:26:40 +0530

    added

--- team-task-manager/backend/server.js
+++ team-task-manager/backend/server.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
+
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/projects', require('./routes/projectRoutes'));
